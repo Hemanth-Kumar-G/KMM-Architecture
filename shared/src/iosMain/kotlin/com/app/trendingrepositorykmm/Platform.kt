5bd2d40f7844c411ef961389707e433e6b9cfaee
Hemanth Kumar <70285394+Hemanth-Kumar-G@users.noreply.github.com>
2023-03-22 12:14:11 +0530

    init

---
+++ shared/src/iosMain/kotlin/com/app/trendingrepositorykmm/Platform.kt
@@ -0,0 +1,9 @@
+package com.app.trendingrepositorykmm
+
+import platform.UIKit.UIDevice
+
+class IOSPlatform: Platform {
+    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+}
+
+actual fun getPlatform(): Platform = IOSPlatform()
